888cef4548c7632945c59eda8df47464032d7677
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-09-29 12:04:23 +0200

    refactor: add numbers auto + fixed build.rs

--- build.rs
+++ build.rs
@@ -6,22 +6,21 @@ fn main() {
     // Get all the files in the src/bin folder and its subfolders
     let files = glob("src/bin/**/*.rs").expect("Failed to read glob pattern");
 
-    // Create a new Cargo.toml file with the bin section
+    // Read the contents of the original Cargo.toml file
+    let original = fs::read_to_string("Cargo.toml").expect("Failed to read Cargo.toml");
+    let mut original = original.split("\n").collect::<Vec<&str>>();
+
+    // Create a new Cargo.toml file with the same contents as the original, but without the bin section
     let mut cargo_toml = fs::File::create("Cargo.toml").expect("Failed to create Cargo.toml");
-    cargo_toml.write_all(b"[package]
-name = \"learning-rust\"
-version = \"0.1.0\"
-edition = \"2021\"
-build = \"build.rs\"
-
-# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
-
-[dependencies]
-glob = \"0.3.1\"
-
-[build-dependencies]
-glob = \"0.3.1\"
-").expect("Failed to write to Cargo.toml");
+    for line in original.iter_mut() {
+        if line == &"[[bin]]" {
+            break;
+        } else {
+            cargo_toml
+                .write_all(format!("{}\n", line).as_bytes())
+                .expect("Failed to write to Cargo.toml");
+        }
+    }
 
     // Loop through the files and append them to the bin section
     for entry in files {
@@ -29,12 +28,28 @@ glob = \"0.3.1\"
             Ok(path) => {
                 // Get the file name without the extension
                 let name = path.file_stem().unwrap().to_str().unwrap();
+
                 // Get the file path as a string
                 let path = path.to_str().unwrap().replace("\\", "/");
+
+                // Get just the number from the folder name
+                let number = path
+                    .split("/")
+                    .nth(2)
+                    .unwrap()
+                    .split(".")
+                    .nth(0)
+                    .unwrap()
+                    .to_string();
+
                 // Write the name and path to the bin section
                 cargo_toml
                     .write_all(
-                        format!("\n[[bin]]\nname = \"{}\"\npath = \"{}\"\n\n", name, path).as_bytes(),
+                        format!(
+                            "[[bin]]\nname = \"{}_{}\"\npath = \"{}\"\n\n",
+                            number, name, path
+                        )
+                        .as_bytes(),
                     )
                     .expect("Failed to write to Cargo.toml");
             }
