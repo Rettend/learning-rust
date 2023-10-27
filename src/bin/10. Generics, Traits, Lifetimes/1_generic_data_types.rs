408f4942183607bcfec971f2831ecc88465a1f98
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-10-27 22:51:48 +0200

    prog: chapter 10

--- src/bin/10. Generics, Traits, Lifetimes/1_generic_data_types.rs
+++ src/bin/10. Generics, Traits, Lifetimes/1_generic_data_types.rs
@@ -24,16 +24,35 @@ fn largest_char(list: &[char]) -> &char {
 
 fn main() {
     // In Function Definitions
-    fn largest<T>(list: &[T]) -> &T {
+    fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
         let mut largest = &list[0];
 
-        for item in list.iter() {
-            // error[E0369]: binary operation `>` cannot be applied to type `T`
-            // if item > largest {
-            //     largest = item;
-            // }
+        for item in list {
+            // restricting T to the PartialOrd trait is needed because of the > operator
+            if item > largest {
+                largest = item;
+            }
         }
 
         largest
     }
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    // In Struct Definitions
+    struct Point<T> {
+        x: T,
+        y: T,
+    }
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
 }
