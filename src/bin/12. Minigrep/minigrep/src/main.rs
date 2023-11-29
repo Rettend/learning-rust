ba8148fa50f11638d213c5084f51c761e38cafb9
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-11-29 11:43:59 +0100

    💜 add lessons 12.1-12.4

---
+++ src/bin/12. Minigrep/minigrep/src/main.rs
@@ -0,0 +1,20 @@
+use minigrep::Config;
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("{err}");
+        process::exit(1);
+    });
+
+    println!("🔎 Searching for \"{}\"", config.query);
+    println!("File: {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
